4989f6dda476182f53aeace0a0cdaf0688288432
slow-groovin <136044792+slow-groovin@users.noreply.github.com>
2025-02-04 21:18:36 +0800

    import impl

--- entrypoints/options/pages/ExportImport.vue
+++ entrypoints/options/pages/ExportImport.vue
@@ -1,54 +1,70 @@
 <script setup lang="ts">
-import {Button} from "@/components/ui/button";
-import {onMounted, ref} from "vue"
-import {db} from "@/utils/client/Dexie";
+import { Button } from "@/components/ui/button";
+import { onMounted, ref } from "vue"
+import { db } from "@/utils/client/Dexie";
 import LoadingOverlay from "@/components/LoadingOverlay.vue";
-import {getIndexedDBUsage} from "@/utils/browser";
-import {useExtTitle} from "@/composables/useExtInfo";
+import { getIndexedDBUsage } from "@/utils/browser";
+import { useExtTitle } from "@/composables/useExtInfo";
+import { CommonFieldChange, CommunityRecord, CommunityTask, HouseChange, HouseTask } from "@/types/lj";
+import { BulkError, EntityTable } from "dexie";
+import { AccessRecord } from "@/utils/lib/AcessRecord";
 
 useExtTitle('导入/导出')
 
-
-const usedMb=ref(0)
+const version = import.meta.env.VITE_EXT_VERSION ?? 'unknown';
+const extName = import.meta.env.VITE_EXT_NAME ?? ''
+const compatibilityVersion = 1
+const usedMb = ref(0)
 const isPending = ref(false)
-
+const importResult=ref<ImportResult>()
+type ExportDataStructure = {
+  version: string,
+  compatibilityVersion: number,
+  lj: {
+    ershoufang: {
+      communities: any[],
+      houses: any[],
+      records: any[],
+      changes: any[],
+      statusChanges: any[],
+      fieldChanges: any[],
+    }
+  },
+}
+type ImportResult = Record<string, { suc: number, fail: number }>
 async function exportAllData() {
   isPending.value = true
 
-  const allC = await db.communityTasks.toCollection().toArray()
-  exportJson('community-tasks.json', allC)
-  
-
-  const allH = await db.houseTasks.toCollection().toArray()
-  exportJson('house-tasks.json', allH)
-  
-
-  const allRecords = await db.communityRecords.toCollection().toArray()
-  exportJson('community-records.json', allRecords)
-  
-
-  const allChanges = await db.houseChanges.toCollection().toArray()
-  exportJson('house-price-changes.json', allChanges)
-  
-
-  const allStatusChanges = await db.houseStatusChanges.toCollection().toArray()
-  exportJson('house-status-changes.json', allStatusChanges)
-  
-
+  const communities = await db.communityTasks.toCollection().toArray()
+  const houses = await db.houseTasks.toCollection().toArray()
+  const records = await db.communityRecords.toCollection().toArray()
+  const changes = await db.houseChanges.toCollection().toArray()
+  const statusChanges = await db.houseStatusChanges.toCollection().toArray()
   const fieldChanges = await db.houseCommonFieldChanges.toCollection().toArray()
-  exportJson('house-common-fields-changes.json', fieldChanges)
-  
 
+  const exportData: ExportDataStructure = {
+    version: version,
+    compatibilityVersion: compatibilityVersion,
+    lj: {
+      ershoufang: {
+        communities: communities,
+        houses: houses,
+        records: records,
+        changes: changes,
+        statusChanges: statusChanges,
+        fieldChanges: fieldChanges
+      }
+    }
+  }
+  exportJson(extName + '-export-' + version + '-' + new Date().toLocaleString() + '.json', exportData)
   isPending.value = false
-
 }
 
 
 function exportJson(filename: string, jsonData: any) {
-  const blob = new Blob([JSON.stringify({
-    version: import.meta.env.VITE_EXT_VERSION,
-    data: jsonData
-  }, null, 2)], {type: 'application/json'});
+
+
+  const blob = new Blob([JSON.stringify(jsonData, null, 2)], { type: 'application/json' });
   const url = URL.createObjectURL(blob);
   const a = document.createElement('a');
   a.href = url;
@@ -57,13 +73,109 @@ function exportJson(filename: string, jsonData: any) {
   URL.revokeObjectURL(url); // 释放 URL 对象
 }
 
+const importPickerOpts = {
+  types: [
+    {
+      description: "JSON",
+      accept: {
+        "application/json": [".json"],
+      },
+    },
+  ],
+  'id': 'hp-ext',
+  startIn: 'downloads',
+  excludeAcceptAllOption: true,
+  multiple: false,
+};
 onMounted(async () => {
-  getIndexedDBUsage().then(rs=>{
-    usedMb.value=rs.usage
+  getIndexedDBUsage().then(rs => {
+    usedMb.value = rs.usage
   })
 })
 
 
+async function importJson() {
+  isPending.value = true
+
+  const _importResult: ImportResult = {}
+  try {
+    //@ts-ignore
+    const [fileHandle] = await window.showOpenFilePicker(importPickerOpts);
+    const file = await fileHandle.getFile();
+    const contents = await file.text();
+
+    const importObj = JSON.parse(contents) as ExportDataStructure
+    if (!importObj.version || !importObj.lj || !importObj.compatibilityVersion) {
+      alert(`导入失败, json文件内容格式非预期`)
+      return
+    }
+    if (importObj.compatibilityVersion !== compatibilityVersion) {
+      alert(`导入失败, json文件的插件版本[${importObj.version}] 与当前版本[${version}] 不兼容`)
+      return
+    }
+
+    /**
+     * community tasks
+     */
+    const communities = importObj.lj.ershoufang.communities as CommunityTask[]
+    _importResult['小区任务'] = await insertRecords(communities, db.communityTasks)
+
+    /**
+     * house tasks
+     */
+    const houses = importObj.lj.ershoufang.houses as HouseTask[]
+    _importResult['房源任务'] = await insertRecords(houses, db.houseTasks)
+
+    const records=importObj.lj.ershoufang.records as CommunityRecord[]
+    _importResult['小区运行历史'] = await insertRecords(records, db.communityRecords)
+
+    _importResult['价格变更'] = await insertRecords(importObj.lj.ershoufang.changes as HouseChange[], db.houseChanges)
+    _importResult['状态变化'] = await insertRecords(importObj.lj.ershoufang.statusChanges as HouseChange[], db.houseStatusChanges)
+    _importResult['房源字段变更'] = await insertRecords(importObj.lj.ershoufang.fieldChanges as CommonFieldChange[], db.houseCommonFieldChanges)
+  } catch (e: any) {
+    if (e.name === 'AbortError') return
+    console.error(e)
+    alert(`导入失败:` + e)
+
+  }
+  importResult.value = _importResult
+  isPending.value = false
+}
+
+const BATCH_SIZE = 10
+/**
+ * 插入导入记录的函数
+ * 分批插入记录
+ * @param records 
+ * @param entityTable 
+ */
+async function insertRecords<T extends { id?: string|number }>(records: T[], entityTable: EntityTable<T, "id">) {
+  //尝试转换特殊字段
+  if ('accessRecord' in records[0]) {
+    records.forEach(_c => {
+      const c = _c as any
+      if (!c?.accessRecord?.bitmap || !c?.accessRecord?.startDate) {
+        return
+      }
+      const newBitMap = Object.values(c.accessRecord.bitmap as object)
+      c.accessRecord = new AccessRecord(c.accessRecord.startDate, Uint32Array.from(newBitMap))
+    })
+  }
+
+  let fail = 0, suc = records.length
+  for (let i = 0; i < records.length; i += BATCH_SIZE) {
+    try {
+      await entityTable.bulkAdd(records.slice(i, i + BATCH_SIZE))
+    } catch (_e: any) {
+      if (_e.name !== 'BulkError') throw _e
+      const e = _e as BulkError
+      fail+= e.failures.length
+    }
+  }
+
+  suc -= fail
+  return { fail, suc }
+}
 </script>
 
 <template>
@@ -74,11 +186,21 @@ onMounted(async () => {
 
   <div class="relative">
     <Button @click="exportAllData">导出</Button>
-    <Button disabled>导入(暂不支持)</Button>
-    <LoadingOverlay v-if="isPending" :delay="0"/>
+    <Button @click="importJson">导入</Button>
+    <LoadingOverlay v-if="isPending" :delay="0" />
+  </div>
+
+  <!-- 导入执行结果 -->
+  <div v-if="importResult" class="flex flex-col gap-4 p-4 mt-4 w-fit rounded-lg shadow-md bg-gray-50 dark:bg-gray-700">
+    <div class="text-lg font-semibold text-green-600 dark:text-green-400">导入完毕!</div>
+    <template v-for="(item, key) in importResult" :key="index">
+      <div class="flex items-center space-x-2">
+        <span class="inline-block bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">{{ key }}</span>
+        <span class="font-semibold text-green-700 mr-8">成功 <span class="font-bold text-green-600">{{ item.suc }}</span> 条</span>
+        <span class="font-semibold text-gray-500">失败 <span class="font-bold text-black">{{ item.fail }}</span> 条</span>
+      </div>
+    </template>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- entrypoints/popup/pages/Index.vue
+++ entrypoints/popup/pages/Index.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 
 
-import {Button} from "@/components/ui/button";
-import {sendMessage} from "@/messaging";
-import {TaskGroup} from "@/types/group";
-import {onMounted, ref, toRaw} from 'vue'
-import {db} from "@/utils/client/Dexie";
-import {browser} from "wxt/browser";
-import {Icon} from "@iconify/vue";
-
+import { Button } from "@/components/ui/button";
+import { sendMessage } from "@/messaging";
+import { TaskGroup } from "@/types/group";
+import { onMounted, ref, toRaw } from 'vue'
+import { db } from "@/utils/client/Dexie";
+import { browser } from "wxt/browser";
+import { Icon } from "@iconify/vue";
+import SimpleTabsContainer from "@/components/layout/SimpleTabsContainer.vue";
+import { useLocalStorage } from "@vueuse/core";
 async function openOptionHomePage() {
   return sendMessage('openOptionPage', '/options.html#/')
 
@@ -23,6 +24,8 @@ const extname = import.meta.env.VITE_EXT_NAME
 const cGroups = ref<TaskGroup[]>([])
 const hGroups = ref<TaskGroup[]>([])
 
+const defaultTabIndex = useLocalStorage('option-tab-index', 0)
+
 async function queryGroups() {
   cGroups.value = await db.communityTaskGroups.toArray()
   hGroups.value = await db.houseTaskGroups.toArray()
@@ -31,10 +34,10 @@ async function queryGroups() {
 async function goBeginCommunityGroupTasks(index: number) {
   const cidList = toRaw(cGroups.value[index].idList)
   const communityList = await db.communityTasks.where('cid').anyOf(cidList).toArray()
-  const id = await db.tempBatchCommunity.add({communityList: communityList})
-  const newWindow = await browser.windows.create({state: 'maximized'})
-  await chrome.sidePanel.open({windowId: newWindow.id as number})
-  await chrome.sidePanel.setOptions({path: '/sidepanel.html#/c/batch?id=' + id})
+  const id = await db.tempBatchCommunity.add({ communityList: communityList })
+  const newWindow = await browser.windows.create({ state: 'maximized' })
+  await chrome.sidePanel.open({ windowId: newWindow.id as number })
+  await chrome.sidePanel.setOptions({ path: '/sidepanel.html#/c/batch?id=' + id })
   db.communityTaskGroups.update(cGroups.value[index].id, {
     lastRunningAt: Date.now()
   })
@@ -43,11 +46,11 @@ async function goBeginCommunityGroupTasks(index: number) {
 
 async function goBeginHouseGroupTasks(index: number) {
   const hidList = toRaw(hGroups.value[index].idList)
-  const id = await db.tempBatchHouse.add({hidList})
+  const id = await db.tempBatchHouse.add({ hidList })
 
-  const newWindow = await browser.windows.create({state: 'maximized'})
-  await chrome.sidePanel.open({windowId: newWindow.id as number})
-  await chrome.sidePanel.setOptions({path: '/sidepanel.html#/h/batch?id=' + id})
+  const newWindow = await browser.windows.create({ state: 'maximized' })
+  await chrome.sidePanel.open({ windowId: newWindow.id as number })
+  await chrome.sidePanel.setOptions({ path: '/sidepanel.html#/h/batch?id=' + id })
   db.houseTaskGroups.update(hGroups.value[index].id, {
     lastRunningAt: Date.now()
   })
@@ -61,44 +64,55 @@ onMounted(() => {
 <template>
 
   <div class="p-2 text-nowrap">
-    <h1 class="flex items-center justify-center w-fit"><span class="text-base">欢迎使用</span> <span
-      class="text-primary">{{ extname }}</span></h1>
-    <div class="flex flex-col gap-2">
-      <Button @click="openOptionHomePage">打开后台页面</Button>
-      <Button @click="openStartUpPage" class="" variant="outline">查看使用说明</Button>
-    </div>
-    <div class="border rounded p-2 my-4 max-h-screen overflow-y-auto">
-      <h2 class="text-xl font-bold text-neutral-500">🟢一键运行任务分组:</h2>
-      <div v-if="cGroups.length" class="mt-8">
-        <h3 class="font-bold text-neutral-500">(小区)任务组:</h3>
-        <div class="flex flex-wrap gap-2">
-          <template v-for="(cGroup,index) in cGroups" :key="index">
-            <Button @click="goBeginCommunityGroupTasks(index)" :disabled="cGroup.idList.length===0" variant="outline"
-                    class="border-2 border-green-500">
-              <Icon icon="solar:play-bold" class="w-4 h-4" />
-              {{ cGroup.name }} ({{ cGroup.idList.length }}个)
-            </Button>
-          </template>
-        </div>
-      </div>
+    <h1 class="flex items-center justify-center mb-2 mt-2">
+      <span class="text-base mx-4">欢迎使用</span>
+      <span class="text-primary">{{ extname }}</span>
+      <img src="/icon/24.png" alt="icon" class="inline">
+    </h1>
 
-      <div v-if="hGroups.length" class="mt-8">
-        <h3 class="font-bold text-neutral-500">(房源)任务组:</h3>
-        <div class="flex flex-wrap gap-2">
-          <template v-for="(hGroup,index) in hGroups">
-            <Button @click="goBeginHouseGroupTasks(index)" :disabled="hGroup.idList.length===0" variant="outline"
-                    class="border-2 border-green-500">
-              <Icon icon="solar:play-bold" class="w-4 h-4" />
-              {{ hGroup.name }} ({{ hGroup.idList.length }}个)
-            </Button>
-          </template>
-        </div>
-      </div>
+    <div class="border rounded p-2 my-4 max-h-screen w-fit min-w-64">
+      <SimpleTabsContainer :initial-tab="defaultTabIndex" @tab-change="(t) => defaultTabIndex = t"
+        :tabs="[{ name: 'default', label: '首页' }, { name: 'group', label: '运行分组' }]">
+        <template #default>
+          <div class="flex flex-col gap-2">
+            <Button @click="openOptionHomePage">打开后台页面</Button>
+            <Button @click="openStartUpPage" class="" variant="outline">查看使用说明</Button>
+          </div>
+        </template>
+        <template #group>
+          <div class="w-fit">
+            <h2 class="text-xl font-bold text-neutral-500">🟢一键运行任务分组:</h2>
+            <div v-if="cGroups.length" class="mt-8">
+              <h3 class="font-bold text-neutral-500">(小区)任务组:</h3>
+              <div class="flex flex-wrap gap-2">
+                <template v-for="(cGroup, index) in cGroups" :key="index">
+                  <Button @click="goBeginCommunityGroupTasks(index)" :disabled="cGroup.idList.length === 0"
+                    variant="outline" class="border-2 border-green-500">
+                    <Icon icon="solar:play-bold" class="w-4 h-4" />
+                    {{ cGroup.name }} ({{ cGroup.idList.length }}个)
+                  </Button>
+                </template>
+              </div>
+            </div>
 
+            <div v-if="hGroups.length" class="mt-8">
+              <h3 class="font-bold text-neutral-500">(房源)任务组:</h3>
+              <div class="flex flex-wrap gap-2">
+                <template v-for="(hGroup, index) in hGroups">
+                  <Button @click="goBeginHouseGroupTasks(index)" :disabled="hGroup.idList.length === 0"
+                    variant="outline" class="border-2 border-green-500">
+                    <Icon icon="solar:play-bold" class="w-4 h-4" />
+                    {{ hGroup.name }} ({{ hGroup.idList.length }}个)
+                  </Button>
+                </template>
+              </div>
+            </div>
+          </div>
+        </template>
+      </SimpleTabsContainer>
     </div>
+
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
